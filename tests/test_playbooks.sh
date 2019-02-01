0cc46638f92a623313981a3bf225698c13f6f9fd
stv <stv@stanford.edu>
2019-01-31 21:55:08 -0800

    Try test stanford roles

--- tests/test_playbooks.sh
+++ tests/test_playbooks.sh
@@ -11,6 +11,18 @@ EOF
 for role_dir in $ROLE_DIRS; do
     echo "    - $(basename $role_dir)" >> travis-test.yml
 done
+cd ../stanford
+ROLE_DIRS=$(/bin/ls -d roles/*)
+cd ../playbooks
+cat <<EOF >travis-test.yml
+- name: Play to test all roles
+  hosts: all
+  roles:
+EOF
+for role_dir in $ROLE_DIRS; do
+    echo "    - $(basename $role_dir)" >> travis-test.yml
+done
+# TODO: test stanford roles
 
 cat travis-test.yml
 echo
