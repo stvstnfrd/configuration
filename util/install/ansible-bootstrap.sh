90acf5665fcac1d6a0b03f3b3688d7b9da5dbc24
stv <stv@stanford.edu>
2019-02-06 12:26:50 -0800

    Update sandbox scripts

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -22,11 +22,11 @@ if [[ -z "${ANSIBLE_VERSION}" ]]; then
 fi
 
 if [[ -z "${CONFIGURATION_REPO}" ]]; then
-  CONFIGURATION_REPO="https://github.com/Stanford-Online/configuration.git"
+  CONFIGURATION_REPO="https://github.com/stvstnfrd/configuration.git"
 fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
-  CONFIGURATION_VERSION="master"
+  CONFIGURATION_VERSION="hawthorn/wip"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
